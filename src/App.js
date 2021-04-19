222876b50959a9e647b08197be34017b7fbb9876
Frank Foti <FFoti@rationalenterprise.com>
2021-04-19 08:58:30 -0400

    trying feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> One now 55555? .
+          Edit <code>src/App.js</code> One now 78789987? .
         </p>
         <a
           className="App-link"
